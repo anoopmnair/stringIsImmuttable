5f2bf889aa8eeb71567529dcc04801172ea4e165
anoopmnair <aswin@aswin-Aspire-A515-51G>
2019-11-05 23:08:46 +0530

    Method added

--- src/main/java/com/anoop/kt/StringIsImmuttable/StringDemo.java
+++ src/main/java/com/anoop/kt/StringIsImmuttable/StringDemo.java
@@ -5,10 +5,19 @@ package com.anoop.kt.StringIsImmuttable;
  *Class to demonstrate String is immutable
  */
 public class StringDemo {
-
+	
+	public void demo()
+	{
+	String str1="Hello";//here str is not an object ,it is a reference to the object
+	String str2=str1;	// str1 and str2 now point at the same string - "Hello"
+	str1="help";//now the reference changed
+	System.out.println(str2);//output is Hello
+	}
+	
 	public static void main(String[] args) {
 		
-
+		StringDemo demo=new StringDemo();
+		demo.demo();
 	}
 
 }
