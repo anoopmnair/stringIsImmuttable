b7e7eb3355e6352b2dda6713962110c5552a6f6a
anoopmnair <aswin@aswin-Aspire-A515-51G>
2019-11-05 23:22:56 +0530

    code changed

--- src/main/java/com/anoop/kt/StringIsImmuttable/StringDemo.java
+++ src/main/java/com/anoop/kt/StringIsImmuttable/StringDemo.java
@@ -12,6 +12,12 @@ public class StringDemo {
 	String str2=str1;	// str1 and str2 now point at the same string - "Hello"
 	str1="help";//now the reference changed
 	System.out.println(str2);//output is Hello
+	
+
+	String s1 = "Hello";
+	String s2 = s1;
+	s1.replace('e', 'a'); // Fictional method that sets character at a given pos in string
+	System.out.println(s2); // Prints "Hallo"
 	}
 	
 	public static void main(String[] args) {
